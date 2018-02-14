b1f2d7d2e937b38ea5408e804b762d7cc3a185b7
celeron1ghz <celeron1ghz@gmail.com>
2018-02-14 04:22:24 +0000

    separated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import request from 'superagent';
 import FontAwesome from 'react-fontawesome';
-import ReactTable from "react-table";
 import { Navbar, Button, Glyphicon } from 'react-bootstrap';
 
 import CircleDescriptionModal from './common/CircleDescriptionModal';
+import CircleListPane from './common/CircleListPane';
 
 class AdminRoot extends React.Component {
   constructor(props, context) {
@@ -94,109 +94,12 @@ class AdminRoot extends React.Component {
           </div>
       }
       <CircleDescriptionModal show={modalShow} circle={selectedCircle} onClose={this.closeModal}/>
+      <CircleListPane
+        circles={circles}
+        onRowClick={this.openModal}
+        onAddFavorite={this.addFavorite}
+        onRemoveFavorite={this.removeFavorite}/>
 
-      <ReactTable
-        className="-striped -highlight"
-        defaultPageSize={20}
-        showPagination={true}
-        data={circles}
-        getTrProps={(a,b) => {
-          return {
-            style: { color: b && b.original && b.original.favorite ? "red" : "" },
-            onClick: () => {
-              this.openModal(b.original);
-            }
-          };
-        }}
-        columns={[
-          {
-            Header: "チェック",
-            headerStyle: { backgroundColor: "#dff", fontSize: "12px" },
-            columns: [
-              {
-                Header: "場所",
-                headerStyle: { backgroundColor: "#ddd", fontSize: "12px" },
-                accessor: "favorite",
-                width: 70,
-                resizable: false,
-                className: "text-center",
-                Cell: row => {
-                  return row.original.favorite
-                    ? <Button bsStyle="danger" bsSize="xs" onClick={e => { e.stopPropagation(); this.removeFavorite(row.original)}}><Glyphicon glyph="minus"/> 削除</Button>
-                    : <Button bsStyle="primary" bsSize="xs" onClick={e => { e.stopPropagation(); this.addFavorite(row.original)  }}><Glyphicon glyph="plus"/> 追加</Button>
-                },
-              }
-            ]
-          },{
-            Header: "サークル情報",
-            headerStyle: { backgroundColor: "#ddf" },
-            columns: [
-              {
-                Header: "場所",
-                headerStyle: { backgroundColor: "#ddd" },
-                accessor: "space_sym",
-                width: 60,
-                resizable: false,
-                className: "text-center",
-              },{
-                Header: "数字",
-                headerStyle: { backgroundColor: "#ddd" },
-                accessor: "space_num",
-                width: 40,
-                resizable: false,
-                className: "text-center",
-              },{
-                Header: "サークル名",
-                headerStyle: { backgroundColor: "#ddd" },
-                accessor: "circle_name",
-                width: 200,
-              },{
-                Header: "作者",
-                headerStyle: { backgroundColor: "#ddd" },
-                accessor: "penname",
-                width: 100,
-              }
-            ]
-          },{
-            Header: "お品書き",
-            headerStyle: { backgroundColor: "#dfd" },
-            columns: [
-              {
-                Header: "コメント",
-                headerStyle: { backgroundColor: "#ddd" },
-                accessor: "circle_comment",
-                width: 300,
-                Cell: row => row.value
-                  ? row.value
-                  : <span style={{ color: "#ccc" }}>(未記入)</span>
-              },{
-                Header: "Link",
-                headerStyle: { backgroundColor: "#ddd" },
-                accessor: "circle_link",
-                className: "text-center",
-                width: 40,
-                resizable: false,
-                Cell: row => row.value
-                  ? <a href={row.value} onClick={e => { e.stopPropagation() }} target="_blank"><Glyphicon glyph="link"/></a>
-                  : ""
-              }
-            ]
-          },{
-            Header: "チェックリスト",
-            headerStyle: { backgroundColor: "#dff" },
-            columns: [
-              {
-                Header: "コメント",
-                headerStyle: { backgroundColor: "#ddd" },
-                accessor: "favorite.comment",
-                width: 300,
-                Cell: row => row.value
-                  ? row.value
-                  : <span style={{ color: "#ccc" }}>(未記入)</span>
-              },
-            ]
-          }
-        ]} />
       {
         this.state.loading &&
           <div className="text-center text-muted">
---
+++ src/common/CircleListPane.js
@@ -0,0 +1,132 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import ReactTable from "react-table";
+import { Glyphicon, Button } from 'react-bootstrap';
+
+class CircleListPane extends React.Component {
+  addFavorite(circle) {
+    this.props.onAddFavorite(circle);
+  }
+
+  removeFavorite(circle) {
+    this.props.onRemoveFavorite(circle)
+  }
+
+  rowClick(circle) {
+    this.props.onRowClick(circle);
+  }
+
+  render() {
+    const { circles } = this.props;
+
+    return <ReactTable
+      className="-striped -highlight"
+      defaultPageSize={20}
+      showPagination={true}
+      data={circles}
+      getTrProps={(a,b) => {
+        return {
+          style: { color: b && b.original && b.original.favorite ? "red" : "" },
+          onClick: () => { this.rowClick(b.original) }
+        };
+      }}
+      columns={[
+        {
+          Header: "チェック",
+          headerStyle: { backgroundColor: "#dff", fontSize: "12px" },
+          columns: [
+            {
+              Header: "場所",
+              headerStyle: { backgroundColor: "#ddd", fontSize: "12px" },
+              accessor: "favorite",
+              width: 70,
+              resizable: false,
+              className: "text-center",
+              Cell: row => {
+                return row.original.favorite
+                  ? <Button bsStyle="danger" bsSize="xs" onClick={e => { e.stopPropagation(); this.removeFavorite(row.original)}}><Glyphicon glyph="minus"/> 削除</Button>
+                  : <Button bsStyle="primary" bsSize="xs" onClick={e => { e.stopPropagation(); this.addFavorite(row.original)  }}><Glyphicon glyph="plus"/> 追加</Button>
+              },
+            }
+          ]
+        },{
+          Header: "サークル情報",
+          headerStyle: { backgroundColor: "#ddf" },
+          columns: [
+            {
+              Header: "場所",
+              headerStyle: { backgroundColor: "#ddd" },
+              accessor: "space_sym",
+              width: 60,
+              resizable: false,
+              className: "text-center",
+            },{
+              Header: "数字",
+              headerStyle: { backgroundColor: "#ddd" },
+              accessor: "space_num",
+              width: 40,
+              resizable: false,
+              className: "text-center",
+            },{
+              Header: "サークル名",
+              headerStyle: { backgroundColor: "#ddd" },
+              accessor: "circle_name",
+              width: 200,
+            },{
+              Header: "作者",
+              headerStyle: { backgroundColor: "#ddd" },
+              accessor: "penname",
+              width: 100,
+            }
+          ]
+        },{
+          Header: "お品書き",
+          headerStyle: { backgroundColor: "#dfd" },
+          columns: [
+            {
+              Header: "コメント",
+              headerStyle: { backgroundColor: "#ddd" },
+              accessor: "circle_comment",
+              width: 300,
+              Cell: row => row.value
+                ? row.value
+                : <span style={{ color: "#ccc" }}>(未記入)</span>
+            },{
+              Header: "Link",
+              headerStyle: { backgroundColor: "#ddd" },
+              accessor: "circle_link",
+              className: "text-center",
+              width: 40,
+              resizable: false,
+              Cell: row => row.value
+                ? <a href={row.value} onClick={e => { e.stopPropagation() }} target="_blank"><Glyphicon glyph="link"/></a>
+                : ""
+            }
+          ]
+        },{
+          Header: "チェックリスト",
+          headerStyle: { backgroundColor: "#dff" },
+          columns: [
+            {
+              Header: "コメント",
+              headerStyle: { backgroundColor: "#ddd" },
+              accessor: "favorite.comment",
+              width: 300,
+              Cell: row => row.value
+                ? row.value
+                : <span style={{ color: "#ccc" }}>(未記入)</span>
+            },
+          ]
+        }
+      ]} />
+  }
+}
+
+CircleListPane.propTypes = {
+  circles: PropTypes.array.isRequired,
+  onRowClick: PropTypes.func.isRequired,
+  onAddFavorite: PropTypes.func.isRequired,
+  onRemoveFavorite: PropTypes.func.isRequired,
+};
+
+export default CircleListPane;
