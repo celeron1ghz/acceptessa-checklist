5deb0b4101aa970c5b83f4e352d79423e052eabe
celeron1ghz <celeron1ghz@gmail.com>
2018-02-15 05:45:41 +0000

    modified style

--- src/common/CirclecutPane.js
+++ src/common/CirclecutPane.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Glyphicon, Button, Col, Image } from 'react-bootstrap';
+import { Label, Glyphicon, Button, Col, Image } from 'react-bootstrap';
 
 class CirclecutPane extends React.Component {
   addFavorite(circle) {
@@ -18,12 +18,12 @@ class CirclecutPane extends React.Component {
   render() {
     const { circles, showChecklistComponent } = this.props;
 
-    return <div style={{ minWidth: "" }}>
+    return <div>
       {
         circles.map(c => {
           const s = c.space_count === "1"
             ? { xs: 12, sm: 6,  md: 4, lg: 3, width: "255px" }
-            : { xs: 12, sm: 12, md: 8, lg: 6, width: "510px" };
+            : { xs: 12, sm: 12, md: 8, lg: 6, Wwidth: "510px" };
 
           return <Col key={c.circlecut} xs={s.xs} sm={s.sm} md={s.md} lg={s.lg}>
             <div style={{ minWidth: s.width, maxWidth: s.width }} onClick={this.imageClick.bind(this,c)}>
@@ -32,29 +32,38 @@ class CirclecutPane extends React.Component {
                 src={c.circlecut}
                 style={{
                   display: "inline-block",
-                  border: c.favorite ? "5px solid aqua" : "5px solid transparent",
+                  height: "100%",
+                  border: c.favorite ? "4px solid aqua" : "4px solid black",
                 }}/>
-              <div style={{ marginTop: "5px", marginBottom: "25px", fontSize: "12px" }}>
-                <Col xs={3} sm={3} md={3} lg={3}>
-                  {c.space_sym} {c.space_num}
-                </Col>
-                <Col xs={6} sm={6} md={6} lg={6}>
+              <div className="clearfix" style={{
+                marginTop: "0px",
+                marginBottom: "25px",
+                marginLeft: "0px",
+                marginRight: "0px",
+                padding: "5px 5px",
+                fontSize: "12px",
+                border: "1px solid gray",
+                height: "60px",
+              }}>
+                <div>
+                  <Label>{c.space_sym}-{c.space_num}</Label>
+                  {' '}
                   {c.circle_name}
-                </Col>
-                {
-                  showChecklistComponent &&
-                    <Col xsPush={3} smPush={3} mdPush={3} lgPush={3} className="text-right">
-                      {
-                        c.favorite
-                          ? <Button bsStyle="danger" bsSize="xs" onClick={e => { e.stopPropagation(); this.removeFavorite(c) }}>
-                              <Glyphicon glyph="minus"/> 削除
-                            </Button>
-                          : <Button bsStyle="primary" bsSize="xs" onClick={e => { e.stopPropagation(); this.addFavorite(c) }}>
-                              <Glyphicon glyph="plus"/> 追加
-                            </Button>
-                      }
-                    </Col>
-                }
+                  {
+                    showChecklistComponent &&
+                      <span className="pull-right">
+                        {
+                          c.favorite
+                            ? <Button bsStyle="danger" bsSize="xs" onClick={e => { e.stopPropagation(); this.removeFavorite(c) }}>
+                                <Glyphicon glyph="minus"/> 削除
+                              </Button>
+                            : <Button bsStyle="primary" bsSize="xs" onClick={e => { e.stopPropagation(); this.addFavorite(c) }}>
+                                <Glyphicon glyph="plus"/> 追加
+                              </Button>
+                        }
+                      </span>
+                  }
+                </div>
               </div>
             </div>
           </Col>;
