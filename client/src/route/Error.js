52ffc46dfc8ceda0c0567ea0e6a98b2f3617213b
celeron1ghz <celeron1ghz@gmail.com>
2018-03-26 04:54:17 +0000

    add error page

---
+++ client/src/route/Error.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Error extends React.Component {
+  render() {
+    return <div className="container">
+      <br/>
+      <h4>404 Not Found</h4>
+      ｴｲｻｧｰｰｲ ﾊﾗﾏｽｺｫｰｰｰｰｲ
+    </div>;
+  }
+}
+
+export default Error;
