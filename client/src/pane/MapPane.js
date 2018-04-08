81cdd712e18cae53fc40bf87f6200854b713a92c
celeron1ghz <celeron1ghz@gmail.com>
2018-04-08 02:50:38 +0000

    add scroll on map image

--- client/src/pane/MapPane.js
+++ client/src/pane/MapPane.js
@@ -30,48 +30,49 @@ class MapPane extends React.Component {
       <div className="text-muted">
         <Glyphicon glyph="exclamation-sign"/> サークルのスペースをクリックすると詳細画面が開きます。
       </div>
-      <div style={{
-        display: "inline-block",
-        border: "1px solid black",
-        height: maps.image_height + "px",
-        width:  maps.image_width + "px",
-        minWidth: "500px",
-        background: `url(${image}) 0 0 no-repeat`,
-        position: "relative" }}>
-        {
-          maps.mappings.map(pos => {
-            const circle = circleIdx[pos.sym + sprintf('%02d', pos.num)];
+      <div className="pre-scrollable" style={{ height: parseInt(maps.image_height) + 10 + "px", maxHeight: parseInt(maps.image_height) + 10 + "px" }}>
+        <div style={{
+          display: "inline-block",
+          border: "1px solid black",
+          height: maps.image_height + "px",
+          width:  maps.image_width + "px",
+          background: `url(${image}) 0 0 no-repeat`,
+          position: "relative" }}>
+          {
+            maps.mappings.map(pos => {
+              const circle = circleIdx[pos.sym + sprintf('%02d', pos.num)];
 
-            if (!circle) {
-              //console.log("NOT FOUND", pos.sym, pos.num);
-              return null;
-            }
+              if (!circle) {
+                //console.log("NOT FOUND", pos.sym, pos.num);
+                return null;
+              }
 
-            const bgColor = circle && favorites[circle.circle_id]
-              ? "rgba(255,0,0,0.3)"
-              : circle
-                ? "rgba(128,128,128,0.7)"
-                : "rgba(64,64,64,0.3)";
+              const bgColor = circle && favorites[circle.circle_id]
+                ? "rgba(255,0,0,0.3)"
+                : circle
+                  ? "rgba(128,128,128,0.7)"
+                  : "rgba(64,64,64,0.3)";
 
-            return <OverlayTrigger
-              key={pos.sym + pos.num}
-              placement="left"
-              overlay={
-                <Tooltip id="tooltip">
-                  {circle ? `${circle.space_sym}-${circle.space_num} : ${circle.circle_name} (${circle.penname})` : ''}
-                </Tooltip>
-              }>
-                <CirclePositionElement
-                  top={pos.top + "px"}
-                  left={pos.left + "px"}
-                  width={pos.width}
-                  height={pos.height}
-                  bgColor={bgColor}
-                  blink={false}
-                  onClick={this.onClick.bind(this,circle)}/>
-            </OverlayTrigger>;
-          })
-        }
+              return <OverlayTrigger
+                key={pos.sym + pos.num}
+                placement="left"
+                overlay={
+                  <Tooltip id="tooltip">
+                    {circle ? `${circle.space_sym}-${circle.space_num} : ${circle.circle_name} (${circle.penname})` : ''}
+                  </Tooltip>
+                }>
+                  <CirclePositionElement
+                    top={pos.top + "px"}
+                    left={pos.left + "px"}
+                    width={pos.width}
+                    height={pos.height}
+                    bgColor={bgColor}
+                    blink={false}
+                    onClick={this.onClick.bind(this,circle)}/>
+              </OverlayTrigger>;
+            })
+          }
+        </div>
       </div>
     </div>;
   }
