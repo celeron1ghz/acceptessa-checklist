bfcbe4957c665676ba42435ed6e680152fb1b3e8
celeron1ghz <celeron1ghz@gmail.com>
2019-04-06 08:25:00 +0900

    fcnize PublicLinkModal.js

--- client/src/modal/PublicLinkModal.js
+++ client/src/modal/PublicLinkModal.js
@@ -1,27 +1,9 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { Label, Button, Glyphicon, Modal, Panel } from 'react-bootstrap';
 import Toggle from 'react-bootstrap-toggle';
 import CopyToClipboard from 'react-copy-to-clipboard';
 
-class PublicLinkModal extends React.Component {
-  constructor(props, context) {
-    super(props, context);
-    this.state = { comment: null };
-    this.close = this.close.bind(this);
-  }
-
-  publicLinkClick(isPublic) {
-    this.props.onPublicLinkClick(isPublic);
-  }
-
-  close() {
-    this.props.onClose();
-  }
-
-  render() {
-    const { show, config, me } = this.props;
-
+export default ({ show, config, me, onPublicLinkClick, onClose }) => {
     if (!config) {
       return <div/>;
     }
@@ -32,7 +14,7 @@ class PublicLinkModal extends React.Component {
       + `?e=${param.get('e')}`
       + `&id=${me ? me.screen_name : ''}`;
 
-    return <Modal show={show} onHide={this.close}>
+    return <Modal show={show} onHide={onClose}>
       <Modal.Header closeButton>
         <Modal.Title>
           <Glyphicon glyph="link"/> チェックリストの公開設定
@@ -50,7 +32,7 @@ class PublicLinkModal extends React.Component {
                 on={<span>公開する</span>}
                 off={<span>公開しない</span>}
                 active={config.public}
-                onClick={this.publicLinkClick.bind(this,!config.public)}/>
+                onClick={onPublicLinkClick.bind(this,!config.public)}/>
             </div>
             <hr/>
             <div>
@@ -82,16 +64,4 @@ class PublicLinkModal extends React.Component {
         <Button block bsStyle="success" onClick={this.close}>閉じる</Button>
       </Modal.Footer>
     </Modal>;
-  }
-}
-
-PublicLinkModal.propTypes = {
-  show: PropTypes.bool,
-  me: PropTypes.object,
-  config: PropTypes.object,
-  //loadings: PropTypes.object.isRequired,
-  onPublicLinkClick: PropTypes.func.isRequired,
-  onClose: PropTypes.func.isRequired,
 };
-
-export default PublicLinkModal;
