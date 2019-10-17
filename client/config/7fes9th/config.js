a5213879130e8b29019a101e1e468c16ef12b8aa
celeron1ghz <celeron1ghz@gmail.com>
2019-10-17 20:45:07 +0900

    add method

--- client/config/7fes9th/config.js
+++ client/config/7fes9th/config.js
@@ -1,25 +1,16 @@
 const _ = require('lodash');
+const sizeOf = require('image-size');
 
-const width = 22;
-const height = 28;
-const common01  = [70, 89, 107, 126, 144, 163, 181, 200, 218, 237, 255, 274, 356, 375, 393, 412];
-const reverse01 = [...common01].reverse();
+function generateConfig(param) {
+  const width = param.width;
+  const height = param.height;
 
-const vertical_syms = [
-  { sym: "ミ", left: 752, tops: [228, 258, 288, 318, 349, 379, 471, 502, 532, 562, 593, 623, 701, 731, 762, 792, 823, 853, 945, 975, 1006, 1035, 1066, 1096] },
-  { sym: "ツ", left: 666, tops: [411, 440, 471, 502, 532, 562, 593, 623, 701, 731, 762, 792, 823, 853, 884, 914, 945, 975, 1006, 1035, 1066, 1096] },
-  { sym: "バ", left: 586, tops: [411, 440, 471, 502, 532, 562, 593, 623, 701, 731, 762, 792, 823, 853, 884, 914, 945, 975, 1006, 1035, 1066, 1096] },
-  { sym: "チ", left: 500, tops: [532, 562, 593, 623, 701, 731, 762, 792, 823, 853, 945, 975, 1006, 1035, 1066, 1096] },
-];
+  const vertical_syms = param.vertical_syms;
+  const horizontal_syms = param.horizontal_syms;
 
-const horizontal_syms = [
-  //{ sym: "シ", top: 315, lefts: common01 },
-];
-
-const mappings = [
-  ..._.flattenDeep([
+  const vertInflated = _.flattenDeep(
     vertical_syms.map(sym => {
-      let idx = sym.idx || 1;
+      const idx = sym.idx || 1;
       return _.zip(_.range(idx, idx + sym.tops.length), sym.tops).map(p => {
         return {
           sym: sym.sym,
@@ -30,10 +21,12 @@ const mappings = [
           height,
         };
       })
-    }),
+    })
+  );
 
+  const horiInflated = _.flattenDeep(
     horizontal_syms.map(sym => {
-      let idx = sym.idx || 1;
+      const idx = sym.idx || 1;
       return _.zip(_.range(idx, idx + sym.lefts.length), sym.lefts).map(p => {
         return {
           sym: sym.sym,
@@ -45,18 +38,43 @@ const mappings = [
         };
       })
     })
-  ]),
-];
+  );
 
-module.exports = {
-  "tweet": {
+  //console.log("inflated vertical   :", vertical_syms.length, "-->", vertInflated.length);
+  //console.log("inflated horizontal :", horizontal_syms.length, "-->", horiInflated.length);
+
+  const mapImagePath = __dirname + "/map.png";
+  const mapMeta =  sizeOf(mapImagePath);
+  const map = {
+    image_width: mapMeta.width,
+    image_height: mapMeta.height,
+    mappings: [...vertInflated, ...horiInflated],
+  };
+
+  console.log("space =", vertInflated.length + horiInflated.length, "image =", mapMeta);
+
+  return { tweet: param.tweet, map };
+}
+
+
+module.exports = generateConfig({
+  width: 22,
+  height: 28,
+
+  vertical_syms: [
+    { sym: "ミ", left: 752, tops: [228, 258, 288, 318, 349, 379, 471, 502, 532, 562, 593, 623, 701, 731, 762, 792, 823, 853, 945, 975, 1006, 1035, 1066, 1096] },
+    { sym: "ツ", left: 666, tops: [411, 440, 471, 502, 532, 562, 593, 623, 701, 731, 762, 792, 823, 853, 884, 914, 945, 975, 1006, 1035, 1066, 1096] },
+    { sym: "バ", left: 586, tops: [411, 440, 471, 502, 532, 562, 593, 623, 701, 731, 762, 792, 823, 853, 884, 914, 945, 975, 1006, 1035, 1066, 1096] },
+    { sym: "チ", left: 500, tops: [532, 562, 593, 623, 701, 731, 762, 792, 823, 853, 945, 975, 1006, 1035, 1066, 1096] },
+  ],
+
+  horizontal_syms: [
+    //{ sym: "シ", top: 315, lefts: common01 },
+  ],
+
+  tweet: {
     "url": "http://7fes.com",
     "hashtags": "ナナフェス",
     "related": "7fes_info",
   },
-  "map": {
-    image_width: "839",
-    image_height: "1377",
-    mappings,
-  },
-};
+});
