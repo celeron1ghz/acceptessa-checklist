2479379e6b7aa7a9af7d9f37649b7d5c42ddfdda
celeron1ghz <celeron1ghz@gmail.com>
2022-05-31 20:51:52 +0900

    concat file

--- client/src/pane/MapPane.js
+++ client/src/pane/MapPane.js
@@ -3,8 +3,39 @@ import PropTypes from 'prop-types';
 import { sprintf } from 'sprintf-js';
 import { OverlayTrigger, Tooltip } from 'react-bootstrap';
 
-import CirclePositionElement from '../common/CirclePositionElement';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import styled from 'styled-components';
+
+const CirclePositionElement = styled.div`
+  @keyframes pointerBlink {
+    0% {
+      opacity: 0.9;
+      transform: scale(1.8, 1.8);
+      border-radius: 50%;
+    }
+    99% {
+      border-radius: 0;
+    }
+    100% {
+      opacity: 0.5;
+      transform: scale(0.9, 0.9);
+      border-radius: 50%;
+    }
+  }
+
+  animation-name: ${props => props.blink ? 'pointerBlink' : ''};
+  animation-duration: 1s;
+  animation-iteration-count: infinite;
+
+  background-color: ${props => props.bgColor};
+  position: absolute;
+  width: ${props => props.width || 5}px;
+  height: ${props => props.height || 5}px;
+  top: ${props => props.top};
+  left: ${props => props.left};
+  cursor: pointer;
+`;
 
 function MapPane({ image, maps, circles, favorites, onCircleClick }) {
   if (!maps) {
@@ -39,7 +70,6 @@ function MapPane({ image, maps, circles, favorites, onCircleClick }) {
               const circle = circleIdx[pos.s + sprintf('%02d', pos.n)];
 
               if (!circle) {
-                //console.log("NOT FOUND", pos.sym, pos.num);
                 return null;
               }
 
