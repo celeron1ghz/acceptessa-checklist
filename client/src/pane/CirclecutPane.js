d0dba3c2b7eb543160fc7ef83fe02bf30b05ef68
celeron1ghz <celeron1ghz@gmail.com>
2022-05-31 20:49:07 +0900

    MapPane migrated to FC

--- client/src/pane/CirclecutPane.js
+++ client/src/pane/CirclecutPane.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from 'react';
+import React from 'react';
 import _ from 'lodash';
 import { Button } from 'react-bootstrap';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
--- client/src/pane/MapPane.js
+++ client/src/pane/MapPane.js
@@ -4,30 +4,24 @@ import { sprintf } from 'sprintf-js';
 import { OverlayTrigger, Tooltip } from 'react-bootstrap';
 
 import CirclePositionElement from '../common/CirclePositionElement';
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-class MapPane extends React.Component {
-  onClick(circle) {
-    this.props.onCircleClick(circle);
+function MapPane({ image, maps, circles, favorites, onCircleClick }) {
+  if (!maps) {
+    return <div />;
   }
 
-  render(){
-    const { circles, favorites, maps, image } = this.props;
+  const circleIdx = {};
 
-    if (!maps) {
-      return <div/>;
+  for (const c of circles) {
+    const numbers = (c.space_num || '').split('-');
+    for (const n of numbers) {
+      circleIdx[c.space_sym + n] = c;
     }
+  }
 
-    const circleIdx = {};
-
-    for (const c of circles) {
-      const numbers = (c.space_num || '').split('-');
-      for (const n of numbers) {
-        circleIdx[c.space_sym + n] = c;
-      }
-    }
-
-    return <div className="">
+  return (
+    <div className="">
       <div className="text-muted mt1e">
         <FontAwesomeIcon icon={['fas', 'info-circle']} /> サークルのスペースをクリックすると詳細画面が開きます。
       </div>
@@ -36,9 +30,10 @@ class MapPane extends React.Component {
           display: "inline-block",
           border: "1px solid black",
           height: maps.image_height + "px",
-          width:  maps.image_width + "px",
+          width: maps.image_width + "px",
           background: `url(${image}) 0 0 no-repeat`,
-          position: "relative" }}>
+          position: "relative"
+        }}>
           {
             maps.mappings.map(pos => {
               const circle = circleIdx[pos.s + sprintf('%02d', pos.n)];
@@ -62,29 +57,21 @@ class MapPane extends React.Component {
                     {circle ? `${circle.space_sym}-${circle.space_num} : ${circle.circle_name} (${circle.penname})` : ''}
                   </Tooltip>
                 }>
-                  <CirclePositionElement
-                    top={pos.t + "px"}
-                    left={pos.l + "px"}
-                    width={pos.w}
-                    height={pos.h}
-                    bgColor={bgColor}
-                    blink={false}
-                    onClick={this.onClick.bind(this,circle)}/>
+                <CirclePositionElement
+                  top={pos.t + "px"}
+                  left={pos.l + "px"}
+                  width={pos.w}
+                  height={pos.h}
+                  bgColor={bgColor}
+                  blink={false}
+                  onClick={() => onCircleClick(circle)} />
               </OverlayTrigger>;
             })
           }
         </div>
       </div>
-    </div>;
-  }
+    </div>
+  )
 }
-
-MapPane.propTypes = {
-  image: PropTypes.string,
-  maps: PropTypes.object,
-  circles: PropTypes.array.isRequired,
-  favorites: PropTypes.object.isRequired,
-  onCircleClick: PropTypes.func.isRequired,
-};
 
 export default MapPane;
