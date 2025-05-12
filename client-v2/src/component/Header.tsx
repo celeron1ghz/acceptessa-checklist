97e3e4981bb0b85d306111fc4ec278a59394e16e
celeron1ghz <celeron1ghz@gmail.com>
2025-05-12 11:25:07 +0900

    adjyst show/hide label by size on header

--- client-v2/src/component/Header.tsx
+++ client-v2/src/component/Header.tsx
@@ -12,9 +12,15 @@ export default function Header(param: { exhibition: Exhibition, count: number })
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href={`#/${param.exhibition.id}/list`}><FontAwesomeIcon icon={faList}/> リストで表示</Nav.Link>
-            <Nav.Link href={`#/${param.exhibition.id}/circlecut`}><FontAwesomeIcon icon={faImage}/> サークルカットで表示</Nav.Link>
-            <Nav.Link href={`#/${param.exhibition.id}/map`}><FontAwesomeIcon icon={faMap}/> マップで表示</Nav.Link>
+            <Nav.Link href={`#/${param.exhibition.id}/list`}>
+              <FontAwesomeIcon icon={faList} /><span className='d-inline d-md-none d-lg-inline'> リストで表示</span>
+            </Nav.Link>
+            <Nav.Link href={`#/${param.exhibition.id}/circlecut`}>
+              <FontAwesomeIcon icon={faImage} /><span className='d-inline d-md-none d-lg-inline'> サークルカットで表示</span>
+            </Nav.Link>
+            <Nav.Link href={`#/${param.exhibition.id}/map`}>
+              <FontAwesomeIcon icon={faMap} /> <span className='d-inline d-md-none d-lg-inline'> マップで表示</span>
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
